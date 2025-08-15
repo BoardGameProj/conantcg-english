3de51d2a251b21c88b9e6f09dc181878ab345379
klingeling <915157650@qq.com>
2025-08-15 18:12:38 +0800

    3

--- website/static/js/deck_list.js
+++ website/static/js/deck_list.js
@@ -72,7 +72,7 @@ function loadDecks() {
 
         // 生成牌组列表HTML
         deckListContainer.innerHTML = `
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 gap-1">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-1">
                 ${decks.map(deck => createDeckCard(deck)).join('')}
             </div>
         `;
