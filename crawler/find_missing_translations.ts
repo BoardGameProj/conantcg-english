65292cda71e746f47bb8cc12045c43fa7b2a0bcc
klingeling <915157650@qq.com>
2025-12-20 15:11:28 +0800

    fix

--- crawler/find_missing_translations.ts
+++ crawler/find_missing_translations.ts
@@ -1,9 +1,15 @@
 import { findMissingFiles } from "./src/find_missing_texts";
 
-const missingFiles = findMissingFiles()
+const missingFiles = findMissingFiles();
+const uniqueKeys = new Set<string>();
+
 for (const missingFile of missingFiles) {
     for (const missingTranslation of missingFile.missingTranslations) {
-        console.warn(`No translation defined for data with id ${missingTranslation.id} (looking for "${missingTranslation.prefix}") ${missingTranslation.ref}`)
+        // console.warn(`No translation defined for data with id ${missingTranslation.id} (looking for "${missingTranslation.prefix}") ${missingTranslation.ref}`)
+        if (missingTranslation.key !== null && !uniqueKeys.has(missingTranslation.key)) {
+            uniqueKeys.add(missingTranslation.key);
+            console.warn(`"${missingTranslation.key}": "",`);
+        }
     }
 }
 
