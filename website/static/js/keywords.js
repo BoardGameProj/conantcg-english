4d38196187823dbe7492efb3890943d661f20739
klingeling <915157650@qq.com>
2025-12-25 07:58:20 +0800

    fix

--- website/static/js/keywords.js
+++ website/static/js/keywords.js
@@ -46,7 +46,7 @@ function processKeywords(text) {
         '灵光一闪': {class: 'dark:text-white text-xs', label: '<img src="img/hirameki.svg" class="inline-icon"><b>$1</b>', tooltip: '作为证据被移除时发动'},
         '变装时': {class: 'bg-fuchsia-400 text-white text-xs px-1 me-1 rounded', tooltip: '此角色使用<span class="text-fuchsia-400 me-1"><img src="img/disguise.svg" class="inline-icon">变装</span>进入现场时发动'},
         '案件浪漫洗牌': {class: 'font-normal bg-pink-600 text-white text-xs px-1 me-1 rounded', tooltip: ''},
-        'MR能力': {class: 'my-4', label: '<b class="bg-black text-white px-2 pt-0.5 pb-2 ring-1 dark:ring-white ring-black rounded-t">MR能力</b>\n<span class="mr-line rounded-b-lg rounded-tr-lg ring-1 dark:ring-white ring-black mb-2"><b class="pl-2">若在对手回合中离开现场，移入搭档区。</b>\n<b class="pl-2">若在我方现场中登场MR，移除此卡牌。</b></b></span>', tooltip: ''},
+        'MR能力': {class: 'my-4', label: '<b class="bg-black text-white px-2 pt-0.5 pb-2 ring-1 dark:ring-white ring-black rounded-t">MR能力</b>\n<span class="mr-line rounded-b-lg rounded-tr-lg ring-1 dark:ring-white ring-black mb-2"><b class="pl-2 text-xs">若在对手回合中离开现场，移入搭档区。</b>\n<b class="pl-2 text-xs">若在我方现场中登场MR，移除此卡牌。</b></b></span>', tooltip: ''},
     }
     for (const keyword in keywords) {
         const config = keywords[keyword]
@@ -125,7 +125,7 @@ function processMechanics(text) {
         '行动': {tooltip: '「行动」对象可以是对手的案件或对手现场中处于休眠状态或眩晕状态的角色'},
         '防卫': {tooltip: '处于激活状态的角色对「行动」进行「防卫」，将进行防卫的角色转为休眠状态'},
         '休眠': {tooltip: '处于休眠状态的角色不能进行行动，可作为对象发生接触'},
-        '眩晕': {tooltip: '处于眩晕状态的角色激活时改为休眠状态'},
+        '眩晕': {tooltip: '处于眩晕状态的角色激活时改为休眠'},
         '刷新': {tooltip: '游戏中，当我方卡组没有卡牌时进行的处理。对手获得1个证据'}
         // 'Investigates?': {tooltip: '对手展示卡组顶指定数量的牌，然后以任意顺序移入卡组底'}
     }
